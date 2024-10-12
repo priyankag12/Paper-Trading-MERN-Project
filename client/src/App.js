3bee1612ebbe8e4ab89c3fd683a28644c3c60d16
Priyanka Gupta <priyankagupta8261@gmail.com>
2024-10-12 21:16:18 +0530

    need to align a little, else done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 import Dashboard from './components/Dashboard/Dashboard';
 import SignIn from './components/Authentication/SignIn';
 import SignUp from './components/Authentication/SignUp';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} /> 
         <Route path="/signup" element={<SignUp />} /> 
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </Router>
   );
