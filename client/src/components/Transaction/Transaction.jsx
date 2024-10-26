beedb1973a1c0be0c5dfb4d342f01c8598f178ae
Pranav Mahajan <pranavmahajan18@gmail.com>
2024-10-26 15:36:35 +0530

    Buy and sell stocks working

--- client/src/components/Transaction/Transaction.jsx
+++ client/src/components/Transaction/Transaction.jsx
@@ -5,83 +5,84 @@ import apiClient from "../../services/apiClient";
 import { getTransactions } from "../../api/transactionApi";
 
 const columns = [
-  {
-    field: "transactionId",
-    headerName: "Transaction ID",
-    flex: 1,
-    minWidth: 110,
-  },
-  { field: "stockName", headerName: "Stock Name", flex: 1, minWidth: 170 },
-  {
-    field: "transactionType",
-    headerName: "Transaction Type",
-    flex: 1,
-    minWidth: 130,
-  },
-  { field: "quantity", headerName: "Quantity", flex: 1, minWidth: 75 },
-  {
-    field: "pricePerShare",
-    headerName: "Price per Share",
-    flex: 1,
-    minWidth: 120,
-  },
-  {
-    field: "totalValue",
-    headerName: "Total Transaction Value",
-    flex: 1,
-    minWidth: 170,
-  },
-  { field: "dateTime", headerName: "Date and Time", flex: 1, minWidth: 140},
+    {
+        field: "transactionId",
+        headerName: "Transaction ID",
+        flex: 1,
+        minWidth: 110,
+    },
+    { field: "stockName", headerName: "Stock Name", flex: 1, minWidth: 170 },
+    {
+        field: "transactionType",
+        headerName: "Transaction Type",
+        flex: 1,
+        minWidth: 130,
+    },
+    { field: "quantity", headerName: "Quantity", flex: 1, minWidth: 75 },
+    {
+        field: "pricePerShare",
+        headerName: "Price per Share",
+        flex: 1,
+        minWidth: 120,
+    },
+    {
+        field: "totalTransactionValue",
+        headerName: "Total Transaction Value",
+        flex: 1,
+        minWidth: 170,
+    },
+    { field: "dateTime", headerName: "Date and Time", flex: 1, minWidth: 140 },
 ];
 
 export default function Transaction() {
-  const [transactions, setTransactions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+    const [transactions, setTransactions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      try {
-        setIsLoading(true);
-        const data = await getTransactions(); 
-        if (data) {
-          setTransactions(data.transactions); 
-        }
-      } catch (error) {
-        console.error("Error fetching transactions:", error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    useEffect(() => {
+        const fetchTransactions = async () => {
+            try {
+                setIsLoading(true);
+                const data = await getTransactions();
 
-    fetchTransactions(); 
-  }, []);
+                if (data) {
+                    setTransactions(data.transactions);
+                }
+            } catch (error) {
+                console.error("Error fetching transactions:", error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
 
-  return (
-    <Box sx={{ overflowX: "auto", maxWidth: "100%" }}>
-      <Typography
-        variant="h4"
-        fontWeight="bold"
-        gutterBottom
-        sx={{ mb: 2, textAlign: "center" }}
-      >
-        Transaction History
-      </Typography>
-      <Box sx={{ width: "100%", minWidth: 600 }}>
-        <DataGrid
-          autoHeight
-          loading={isLoading}
-          rows={transactions} 
-          getRowId={(row) => row._id}
-          columns={columns}
-          disableSelectionOnClick
-          initialState={{
-            pagination: { paginationModel: { pageSize: 20 } },
-          }}
-          pageSizeOptions={[10, 20, 30]}
-          disableColumnResize
-          density="compact"
-        />
-      </Box>
-    </Box>
-  );
+        fetchTransactions();
+    }, []);
+
+    return (
+        <Box sx={{ overflowX: "auto", maxWidth: "100%" }}>
+            <Typography
+                variant="h4"
+                fontWeight="bold"
+                gutterBottom
+                sx={{ mb: 2, textAlign: "center" }}
+            >
+                Transaction History
+            </Typography>
+            <Box sx={{ width: "100%", minWidth: 600 }}>
+                <DataGrid
+                    autoHeight
+                    loading={isLoading}
+                    rows={transactions}
+                    getRowId={(row) => row._id}
+                    columns={columns}
+                    disableSelectionOnClick
+                    initialState={{
+                        pagination: { paginationModel: { pageSize: 20 } },
+                    }}
+                    pageSizeOptions={[10, 20, 30]}
+                    disableColumnResize
+                    density="compact"
+                />
+            </Box>
+        </Box>
+    );
 }
